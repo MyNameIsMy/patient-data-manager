c9ebeb5f6ce0a8383256b1f06db1faba4e486b94
Alex <suchushin.alexey@gmail.com>
2023-04-22 14:09:11 +0200

    Create base project #1

---
+++ src/main/java/sukhushin/projects/patient/data/manager/PatientDataManagerApplication.java
@@ -0,0 +1,13 @@
+package sukhushin.projects.patient.data.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientDataManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientDataManagerApplication.class, args);
+	}
+
+}
